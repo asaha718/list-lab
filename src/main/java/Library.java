0fa2cd05086b7b02fe748da26d7a1507759735df
Anug Saha <anugsaha@gmail.com>
2022-06-28 17:29:18 -0400

    add book logic

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -3,4 +3,8 @@ import java.util.List;
 
 public class Library {
     private static List<Book> listOfBooks = new ArrayList<>();
+
+    public static void addBook(Book book) {
+        Library.listOfBooks.add(book);
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // your code here
+        getBookInput();
     }
+
+    public static void getBookInput() {
+        try (Scanner scanner = new Scanner(System.in)) {
+            String title = "";
+            String genre = "";
+            Integer numberOfPages = 0;
+            System.out.println("Add a Book");
+
+            System.out.print("Enter title: ");
+            title = scanner.nextLine();
+            System.out.print("Enter the Genre: ");
+            genre = scanner.nextLine();
+
+            System.out.print("Please enter the number of pages: ");
+            numberOfPages = scanner.nextInt();
+            scanner.nextLine();
+
+            Book book = new Book(title, genre, numberOfPages);
+            Library.addBook(book);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
